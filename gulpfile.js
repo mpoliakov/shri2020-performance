52e3e0641c67263f80d820d4706060b75139de22
Max Poliakov <mpolyakov146@gmail.com>
2020-04-10 09:48:50 +0500

    svgmin

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,12 @@
-var gulp = require("gulp");
+const gulp = require("gulp");
+const svgmin = require("gulp-svgmin");
+
+gulp.task('svgmin', function () {
+    return gulp.src('img/*.svg')
+        .pipe(svgmin({
+            plugins: [{
+                removeViewBox: false
+            }]
+        }))
+        .pipe(gulp.dest('img'));
+});
