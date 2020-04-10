f8cb9547578569549949978e0f9dd73a592586ef
Max Poliakov <mpolyakov146@gmail.com>
2020-04-10 10:19:15 +0500

    .min.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require("gulp");
 const svgmin = require("gulp-svgmin");
 const csso = require("gulp-csso");
 const rename = require("gulp-rename");
+const uglify = require("gulp-uglify");
 
 gulp.task('svgmin', function () {
     return gulp.src('img/*.svg')
@@ -20,3 +21,9 @@ gulp.task("csso", function () {
         .pipe(gulp.dest("css"));
 });
 
+gulp.task('uglify', function () {
+    return gulp.src('js/*.js')
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('js'));
+});
